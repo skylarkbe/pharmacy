51dcbb18c09850ae639caf5ed6faa6adcfe67631
skylarkbe <45628648+skylarkbe@users.noreply.github.com>
2019-02-14 12:01:55 +0100

    Update data model (add inheritance and stringify methods)

--- inventory/models.py
+++ inventory/models.py
@@ -3,17 +3,27 @@ from django.db import models
 
 class Pharmaceutical(models.Model):
     name = models.CharField(max_length=256)
-    image = models.ImageField
     medicalPrescription = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
 
-class Pill(models.Model):
+class Medicine(models.Model):
     type = models.ForeignKey(Pharmaceutical, on_delete=models.CASCADE)
-    insertDate = models.DateField
-    expirationDate = models.DateField
+    insertDate = models.DateField(auto_now_add=True)
+    expirationDate = models.DateField(auto_now_add=True)
+
+
+class Pill(Medicine):
+    def __str__(self):
+        return "(ID:" + self.id.__str__() + ") " + self.type.__str__() + " Exp.: " + self.expirationDate.__str__()
+
+
+class Syrup(Medicine):
+    openedDate = models.DateField()
+    validity = models.IntegerField()
 
     def __str__(self):
-        return self.type
+        return "(ID:" + self.id.__str__() + ") " + self.type__str__() + " Exp.: " + self.expirationDate.__str__() + \
+               " Opened: " + self.expirationDate.__str__()
