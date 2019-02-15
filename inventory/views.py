13723d13435698a47d2f17d963affd901c88d369
Canter Martin <martin.canter@ominatechnologies.com>
2019-02-15 15:37:08 +0100

    1st commit test + exo

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from .models import Syrup
 
 
@@ -7,3 +7,6 @@ def index(request):
     all_medicines = Syrup.objects.all()
     context = {'all_medicines': all_medicines, }
     return render(request, 'inventory/index.html', context)
+
+def addPharmaceutical(request):
+    return HttpResponse("Hello world, you're at the add pharmaceutical page")
